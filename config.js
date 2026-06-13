dee737d97599b3fed0bf2882051ab7c8d2a5b0de
ehtishamrk <founder@aolfs.com>
2026-06-14 02:00:42 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,4 +5,4 @@
 // ============================================================
 
 const SUPABASE_URL     = 'https://zxgwzhvolcsqktvgrogy.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_WomLIY7EG1Jq6y76tHxFMQ_b5CiYVNI';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp4Z3d6aHZvbGNzcWt0dmdyb2d5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEzNjIxMjYsImV4cCI6MjA5NjkzODEyNn0.D5vyyPcvYw3GuO9n_sNqB-h0Z0JZFBBS_PMYZkAeukY';
